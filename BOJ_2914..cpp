ea5444bc27f21dbd4dfad2ecc6413312e243c622
meantint <mistinca@naver.com>
2020-07-02 19:28:25 +0900

    Create BOJ_2914

---
+++ BOJ_2914..cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        int a, i;
+        cin >> a >> i;
+
+        int answer = a * (i - 1) + 1;
+        cout << answer << '\n';
+
+        return 0;
+}
