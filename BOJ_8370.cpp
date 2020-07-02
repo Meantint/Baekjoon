a2bf44b4d587d3819f0181f4c9e66a9fc948dfd6
Meantint <meantint@gmail.com>
2020-07-02 19:55:11 +0900

    Create BOJ_8370

---
+++ BOJ_8370.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        int n1, k1;
+        int n2, k2;
+        cin >> n1 >> k1;
+        cin >> n2 >> k2;
+
+        int answer = n1 * k1 + n2 * k2;
+        cout << answer << '\n';
+
+        return 0;
+}
