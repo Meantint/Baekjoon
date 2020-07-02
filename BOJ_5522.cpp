45a3a1a332e17b650de928b5c75ca5fb57bb69a6
Meantint <meantint@gmail.com>
2020-07-02 19:48:41 +0900

    Create BOJ_5522

---
+++ BOJ_5522.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        int num;
+        int answer = 0;
+
+        for (int i = 0; i < 5; ++i) {
+                cin >> num;
+                answer += num;
+        }
+        cout << answer << '\n';
+
+        return 0;
+}
