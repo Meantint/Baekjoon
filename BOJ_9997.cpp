2ecf6a68dcb07c2ca61db24ca802ae9ab9ce3cf2
meantint <mistinca@naver.com>
2020-07-31 15:33:38 +0900

    Update BOJ

--- BOJ_9997.cpp
+++ BOJ_9997.cpp
@@ -7,34 +7,35 @@ int all = (1 << 26) - 1;
 int answer = 0;
 string st[25];
 
-void dfs(int cur, int stat){
-	if(stat == all){
-		answer += pow(2, n - cur);
-		return;
-	}
-	if(cur == n){
-		return;
-	}
+void dfs(int cur, int stat)
+{
+        if (stat == all) {
+                answer += pow(2, n - cur);
+                return;
+        }
+        if (cur == n) {
+                return;
+        }
 
-	int cp_stat = stat;
-	int size = st[cur].size();
-	for(int i = 0; i < size; ++i){
-		cp_stat |= (1 << (st[cur][i] - 'a'));
-	}
+        int cp_stat = stat;
+        int size = st[cur].size();
+        for (int i = 0; i < size; ++i) {
+                cp_stat |= (1 << (st[cur][i] - 'a'));
+        }
 
-	dfs(++cur, cp_stat);
-	dfs(cur, stat);
+        dfs(++cur, cp_stat);
+        dfs(cur, stat);
 }
 
 int main()
 {
-	cin >> n;
+        cin >> n;
 
-	for(int i = 0; i < n; ++i){
-		cin >> st[i];
-	}
-	dfs(0, 0);
-	cout << answer << '\n';
+        for (int i = 0; i < n; ++i) {
+                cin >> st[i];
+        }
+        dfs(0, 0);
+        cout << answer << '\n';
 
-	return 0;
+        return 0;
 }
