d22b1b1eb303d9af787902a977d77a9b4a12d541
meantint <mistinca@naver.com>
2020-07-31 21:05:14 +0900

    Create BOJ_9084

--- BOJ_9084.cpp
+++ BOJ_9084.cpp
@@ -2,39 +2,38 @@
 
 using namespace std;
 
-int t, n, m;
+int n, m, t;
 int cost[20];
-int dp[21][10010];
+int dp[10010];
 
 int main()
 {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-        cin >> t;
-        for (int tc = 0; tc < t; ++tc) {
-                memset(cost, 0, sizeof(cost));
-                memset(dp, 0, sizeof(dp));
-                cin >> n;
-
-                for (int i = 0; i < n; ++i) {
-                        cin >> cost[i];
-                }
-                cin >> m;
-                for (int i = 1; i <= n; ++i) {
-                        for (int j = 1; j <= m; ++j) {
-                                if (j % cost[i - 1] == 0)
-                                        if (j - cost[i - 1] >= 0)
-                                                dp[i][j] = dp[i][j - cost[i - 1]] + dp[i - 1][j];
-                        }
-                }
-                for (int i = 1; i <= n; ++i) {
-                        for (int j = 1; j <= m; ++j) {
-                                cout << dp[i][j] << ' ';
-                        }
-                        cout << '\n';
-                }
+    cin >> t;
+    for (int tc = 0; tc < t; ++tc) {
+        memset(cost, 0, sizeof(cost));
+        memset(dp, 0, sizeof(dp));
+        cin >> n;
+        for (int i = 0; i < n; ++i) {
+            cin >> cost[i];
         }
+        cin >> m;
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                if (j + cost[i] <= m)
+                    dp[j + cost[i]] += dp[j];
+            }
+        }
+        /*
+        for (int i = 0; i <= m; ++i) {
+            cout << dp[i] << ' ';
+        }
+        */
+        cout << dp[m] << '\n';
+    }
 
-        return 0;
+    return 0;
 }
