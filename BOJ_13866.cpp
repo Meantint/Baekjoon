f653b7cdfe6fa19db7d141f65461edfe06cbeb95
meantint <mistinca@naver.com>
2020-08-27 14:42:48 +0900

    Create BOJ_13866

---
+++ BOJ_13866.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vec(4);
+    for (int i = 0; i < 4; ++i) {
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    cout << abs(vec[0] + vec[3] - vec[1] - vec[2]) << '\n';
+
+    return 0;
+}
