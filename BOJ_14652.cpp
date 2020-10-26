18e5f2f7de78f90aa0664fba45e8c0a316197182
meantint <mistinca@naver.com>
2020-10-26 23:22:29 +0900

    Create BOJ_14652

---
+++ BOJ_14652.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, k;
+
+int main()
+{
+    cin >> n >> m >> k;
+    cout << k / m << ' ' << k % m << '\n';
+
+    return 0;
+}
