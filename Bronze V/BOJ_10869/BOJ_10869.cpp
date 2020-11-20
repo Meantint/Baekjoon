9fb2acde76cfa352e417179de40a4c313b648cf4
meantint <mistinca@naver.com>
2020-11-20 15:56:42 +0900

    Create BOJ_10869 (cpp & md files)

---
+++ Bronze V/BOJ_10869/BOJ_10869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+    cin >> a >> b;
+
+    cout << a + b << '\n'
+         << a - b << '\n'
+         << a * b << '\n'
+         << a / b << '\n'
+         << a % b << '\n';
+
+    return 0;
+}
