53dd75fccbcbefb37fe4e6d83690000dd71511d0
meantint <mistinca@naver.com>
2020-12-02 13:44:20 +0900

    Create BOJ_1927 (cpp & md files)

--- Silver I/BOJ_1927/BOJ_1927.cpp
+++ Silver I/BOJ_1927/BOJ_1927.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 
 using namespace std;
+
+int n, x;
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
     cin >> n;
-
-    priority_queue<int, vector<int>, greater<int>> pq;
-
-    for (int i = 0; i < n; ++i) {
-        int x;
+    while (n--) {
         cin >> x;
         if (x == 0) {
             if (pq.empty()) {
                 cout << '0' << '\n';
-
-                continue;
             }
-            cout << pq.top() << '\n';
-            pq.pop();
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
         }
         else {
             pq.push(x);
